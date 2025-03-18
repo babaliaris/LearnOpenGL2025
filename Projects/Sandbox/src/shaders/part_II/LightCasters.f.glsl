746b819f5bc15ad3668b01e34f6c2e319c615232
babaliaris <babaliaris.nikos@gmail.com>
2025-03-18 23:36:15 +0200

    Cleaned up function signatures

--- Projects/Sandbox/src/shaders/part_II/LightCasters.f.glsl
+++ Projects/Sandbox/src/shaders/part_II/LightCasters.f.glsl
@@ -44,31 +44,31 @@ void main()
     fColor = calculateFinalColor();
 }
 
-vec4 calculateAmbient(in vec4 diffuseMap, in vec3 lightColor, float lightSrength)
+vec4 calculateAmbient(in vec4 diffuseMap, in Light light)
 {
-    return vec4(vec3(diffuseMap) * lightColor * lightSrength, 1.0f);
+    return vec4(vec3(diffuseMap) * light.color * light.strength, 1.0f);
 }
 
-vec4 calculateDiffuse(in vec4 diffuseMap, in vec3 lightColor, in float lightStrength, in vec3 lightDir, in vec3 normalDir)
+vec4 calculateDiffuse(in vec4 diffuseMap, in Light light, in vec3 lightDir, in vec3 normalDir)
 {
     float diffStrength = max(dot(-lightDir, normalDir), 0.0f);
 
-    return vec4(vec3(diffuseMap) * lightColor * lightStrength * diffStrength, 1.0f);
+    return vec4(vec3(diffuseMap) * light.color * light.strength * diffStrength, 1.0f);
 }
 
 
-vec4 calculateSpecular(in vec4 specularMap, in vec3 lightColor, in float lightStrength, in vec3 lightDir, in vec3 normalDir, in vec3 eyeDir, int shininess)
+vec4 calculateSpecular(in vec4 specularMap, in Light light, in vec3 lightDir, in vec3 normalDir, in vec3 eyeDir)
 {
     vec3 reflectDir = normalize(reflect(lightDir, normalDir));
 
-    float specStrength = pow(max(dot(reflectDir, -eyeDir), 0.0f), shininess);
+    float specStrength = pow(max(dot(reflectDir, -eyeDir), 0.0f), uMat.shininess);
 
-    return vec4( vec3(specularMap) * lightColor * lightStrength * specStrength, 1.0f);
+    return vec4( vec3(specularMap) * light.color * light.strength * specStrength, 1.0f);
 }
 
-float calcAttenuation(in float d, in float kc, in float kl, in float kq)
+float calcAttenuation(in float d, in Light light)
 {
-    return 1.0f/( kc + kl*d + kq*pow(d,2) );
+    return 1.0f/( light.kc + light.kl*d + light.kq*pow(d,2) );
 }
 
 
@@ -98,7 +98,7 @@ vec4 calculateFinalColor()
         {
             float spotItense  = 1.0f;
             float lightDist   = length(fragPos - light.position);
-            float attenuation = calcAttenuation(lightDist, light.kc, light.kl, light.kq);
+            float attenuation = calcAttenuation(lightDist, light);
             vec3  lightDir    = normalize(fragPos - light.position);
 
             //If it is also a spot light, add circle mode intensity.
@@ -109,8 +109,8 @@ vec4 calculateFinalColor()
             }
 
             //Calculating diffuse, specular and adding attenuation, is the same for both point and spot lights.
-            vec4 diffuse  = calculateDiffuse(diffuseMap, light.color, light.strength, lightDir, normalDir);
-            vec4 specular = calculateSpecular(specularMap, light.color, light.strength, lightDir, normalDir, eyeDir, uMat.shininess);
+            vec4 diffuse  = calculateDiffuse(diffuseMap, light, lightDir, normalDir);
+            vec4 specular = calculateSpecular(specularMap, light, lightDir, normalDir, eyeDir);
 
             finalColor  += (diffuse + specular) * attenuation * spotItense;
         }
@@ -120,7 +120,7 @@ vec4 calculateFinalColor()
         {   
             //Keep ambient total light in a seperate variable, because if it is the last
             //color added to the finalColor, then we make sure there is always some light!
-            ambientColor += calculateAmbient(diffuseMap, light.color, light.strength);
+            ambientColor += calculateAmbient(diffuseMap, light);
         }
 
         //Break out of the loop if light is the last light.
